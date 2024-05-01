8f66ac7d4e2d844079d014c4345d48733067c6dc
Pietro Bucker <pietro_177@hotmail.com>
2024-05-01 17:59:15 -0300

    refactor: feito a funçao no helper para pegar dados

--- helper.php
+++ helper.php
@@ -1,9 +1,8 @@
 <?php
-include 'requisicao.php';
-
+function getDados($data) {
     //acesso aos valores da panilha
-    $valores = $resposta->values;
-
+    $valores = $data->values;
+    
     //excluindo quem ja saiu da empresa
     $valores_filtrados = array_filter($valores, function($linha) {
         return $linha[3] === null;
@@ -11,4 +10,6 @@ include 'requisicao.php';
     
     //pegando em forma de coluna os dados
     $dados = array_column($valores_filtrados, null);
+    return $dados;
+}
 ?>
