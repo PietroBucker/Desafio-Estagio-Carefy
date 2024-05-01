ce158162b1aede292acea8ede1d4ede080585a49
Pietro Bucker <pietro_177@hotmail.com>
2024-05-01 18:29:15 -0300

    styled: feito estilizaçao parcial

--- index.php
+++ index.php
@@ -16,8 +16,29 @@ $dados = getDados($data);
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="styles.css">
 </head>
 <body>
-    
+    <main>
+
+        <h1>Aniversarios Na Empresa</h1>
+        <table>
+            <thead>
+                <tr>
+                    <th>Nome</th>
+                    <th>Aniversario</th>
+                    <th>Aniversario de Empresa</th>
+                </tr>
+            </thead>
+            <?php foreach ($dados as $dado) : ?>
+                <tbody>
+                    <tr>
+                        <td><?= $dado[0] ?></td>
+                        <td><?= $dado[1] ?></td>
+                        <td><?= $dado[2] ?></td>
+                    </tr>
+                </tbody>
+                    <?php endforeach; ?>
+    </main>
 </body>
 </html>
