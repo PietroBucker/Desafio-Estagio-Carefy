1cebc1841b27181c7175cbe479921eab9ca6520b
Pietro Bucker <pietro_177@hotmail.com>
2024-05-01 17:58:17 -0300

    fix: corrigido nome do arquivo

--- requisicao.php
+++ requisicao.php
@@ -1,17 +1,17 @@
 <?php
     //Chave de acesso para api
-    $chave = "sua_chave_de_acesso";
+    $chave = "sua chave aqui";
 
     //ID da planilha do Google Sheets
-    $id = "sua_id_da_planilha";
+    $id = "1-fWXw-8tuKHiDdaSmNIUnNcRrcah2D8Bw_zaME0HxpM";
 
     //Nome da aba da planilha
-    $aba = "sua_aba_da_planilha";
+    $aba = "Página1";
     //intervalo das células da planilha
-    $intervalo = "A1:Z1000";
+    $intervalo = "A1:D20";
 
     //URL da API do Google Sheets
-    $url = "https://sheets.googleapis.com/v4/spreadsheets/$spreadsheet_id/values/$sheet_name!$intervalo?key=$api_key";
+    $url = "https://sheets.googleapis.com/v4/spreadsheets/$id/values/$aba!$intervalo?key=$chave";
 
 
     //Criação da requisição
@@ -19,17 +19,18 @@
 
     //Configuração da requisição
     curl_setopt($requisicao, CURLOPT_URL, $url);
-    curl_setopt($requisicao, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($requisicao, CURLOPT_RETURNTRANSFER, 1);
     curl_setopt($requisicao, CURLOPT_SSL_VERIFYPEER, false);
 
     //Execução da requisição
     $resposta = curl_exec($requisicao);
 
+    if (curl_errno($requisicao)) {
+        echo 'Erro ao fazer a requisição: ' . curl_error($requisicao);
+    }
     //Fechamento da requisição
     curl_close($requisicao);
 
     //Decodificação da resposta
-    $resposta = json_decode($resposta);
-    
-
+    $data = json_decode($resposta);
 ?>
